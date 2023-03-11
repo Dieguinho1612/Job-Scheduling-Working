a8d1a51ce797e255ca7a6289c1a7cd2a5dd51d68
hitzges <hitzgesdiego@yahoo.de>
2023-03-11 16:01:53 +0100

    Jobanzahl in Speichernamen

--- JobScript_for_higher_data_generation.py
+++ JobScript_for_higher_data_generation.py
@@ -522,7 +522,7 @@ input = sys.argv[1]
 
 def save_dictionary(data_dictionary):
     #with open(f'D:\\Job-Scheduling-Files\Data\EstimData\estim_data_{input}.pickle', 'wb') as f:
-    with open(f'EstimData/estim_data_{input}.pickle', 'wb') as f:
+    with open(f'EstimData/estim_data_{n}_Jobs_{input}.pickle', 'wb') as f:
         pickle.dump(data_dictionary, f, pickle.HIGHEST_PROTOCOL)
 
 
